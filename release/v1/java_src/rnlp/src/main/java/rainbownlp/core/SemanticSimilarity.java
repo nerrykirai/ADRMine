2640d0df12dd66012c8050533cd95d2ac1f2d4ba
nerrykirai <3757640@mail.ru>
2020-05-13 18:37:18 +0500

    sum1

--- release/v1/java_src/rnlp/src/main/java/rainbownlp/core/SemanticSimilarity.java
+++
@@ -1,9 +0,0 @@
-package rainbownlp.core;
-
-public class SemanticSimilarity {
-
-	public static double getSimilarity(String concept1, String concept2) {
-		return (concept1.equals(concept2))?1:0;
-	}
-
-}
--- release/v1/java_src/rnlp/src/main/java/rainbownlp/machineLearning/IFeatureCalculator.java
+++
@@ -1,7 +0,0 @@
-package rainbownlp.machineLearning;
-
-import java.sql.SQLException;
-
-public interface IFeatureCalculator {
-	public void calculateFeatures(MLExample exampleToProcess) throws SQLException, Exception;
-}
